e7195c0a834e1f42e00f7281ce8bb5e59d23f45f
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-02-20 18:25:09 +0545

    Scripts File

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,42 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+    if (loginForm) {
+        loginForm.addEventListener("submit", function (e) {
 
-    loginForm.addEventListener("submit", function(e) {
-        const username = document.getElementById("username").value.trim();
-        const password = document.getElementById("password").value.trim();
+            const phone = document.getElementById("phone").value.trim();
+            const password = document.getElementById("password").value.trim();
 
-        if(username === "" || password === "") {
-            alert("Please enter both username and password.");
-            e.preventDefault();
-        }
-    });
+            if (phone === "" || password === "") {
+                alert("Please enter both phone and password.");
+                e.preventDefault();
+            }
+        });
+    }
+    if (registerForm) {
+        registerForm.addEventListener("submit", function (e) {
+
+            const password = document.getElementById("password").value;
+            const confirmPassword = document.getElementById("confirmPassword").value;
+
+            if (password !== confirmPassword) {
+                alert("Passwords do not match!");
+                e.preventDefault();
+            }
+        });
+    }
+
 });
-const registerForm = document.getElementById("registerForm");
-
-if (registerForm) {
-
-    registerForm.addEventListener("submit", function(event) {
-
-        const passwordInput = document.getElementById("password");
-        const confirmPasswordInput = document.getElementById("confirmPassword");
-
-        const password = passwordInput.value;
-        const confirmPassword = confirmPasswordInput.value;
-        let oldError = document.querySelector(".error-message");
-        if (oldError) {
-            oldError.remove();
-        }
-        confirmPasswordInput.style.border = "1px solid #aaa";
-
-        if (password !== confirmPassword) {
-            event.preventDefault();
-            confirmPasswordInput.style.border = "1px solid red";
-            const error = document.createElement("div");
-            error.className = "error-message";
-            error.textContent = "Passwords do not match!";
-
-            confirmPasswordInput.parentElement.appendChild(error);
-        }
-    });
-}
