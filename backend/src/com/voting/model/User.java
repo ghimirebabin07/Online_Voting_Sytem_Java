5a89aeefb58f5164c80c7c8260571972080f4019
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-03-03 21:31:01 +0545

    Initial folders

---
+++ backend/src/com/voting/model/User.java
@@ -0,0 +1,12 @@
+package com.voting.model;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+    private boolean voted;
+
+    // Getters and Setters
+}
