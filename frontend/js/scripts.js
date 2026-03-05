d9b7bcae9056ae886c802fecd71a53143eba5215
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-03-05 23:51:10 +0545

    JS

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -34,7 +34,7 @@ if (!localStorage.getItem("candidates")) {
             image: "../Images/Sobita.jpg",
             symbol: "../Images/RSP.jpg",
             votes: 0,
-            description: "Well educated and focused on women empowerment and policy reform."
+            description: "Well educated and focused on women empowerment and policy reform with a commitment to social justice."
         },
         {
             id: 4,
